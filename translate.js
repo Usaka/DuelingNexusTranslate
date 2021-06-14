8c813b82b3321dcadeabd79a35a794201c13b96a
Usaka Rokujou <usaka.rokujou@gmail.com>
2021-06-13 22:14:20 -0500

    Update sintaxis

--- translate.js
+++ translate.js
@@ -8,97 +8,93 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+function changeLanguage(){
+    localStorage.setItem('language', select.value)
+    language = select.value
+    let temp = lastCurrentCardId
+    lastCurrentCardId = 0
+    Engine.ui.setCardInfo(temp)
+}
 
-    function changeLanguage(){
-        localStorage.setItem('language', select.value)
-        language = select.value
-        let temp = lastCurrentCardId
-        lastCurrentCardId = 0
-        Engine.ui.setCardInfo(temp)
-    }
+function loadLanguage(a){
+    if (!(0 >= a) && lastCurrentCardId !== a) {
+        lastCurrentCardId = a
+        document.getElementById("card-name").innerHTML = 'Wait...'
+        document.getElementById("card-description").innerHTML = 'Wait...'
 
-    function loadLanguage(a){
-        if (!(0 >= a) && lastCurrentCardId !== a) {
-            lastCurrentCardId = a
-            document.getElementById("card-name").innerHTML = 'Wait...'
-            document.getElementById("card-description").innerHTML = 'Wait...'
+        if(cardsTemp[a]){
+            document.getElementById("card-name").innerHTML = cardsTemp[a][language].name
+            document.getElementById("card-description").innerHTML = cardsTemp[a][language].lore
+        }else{
+            a = Engine.getCardData(a)
+            const name = a.name
+            // achichi%20@Ignister
+            fetch(`https://yugioh.fandom.com/api.php?action=query&titles=${name}&prop=revisions&rvslots=*&rvprop=content&formatversion=2&format=json&origin=*`)
+                .then(res => res.json())
+                .then(result => {
+                var content = result.query?.pages[0]?.revisions[0]?.slots?.main?.content
 
-            if(cardsTemp[a]){
-                document.getElementById("card-name").innerHTML = cardsTemp[a][language].name
-                document.getElementById("card-description").innerHTML = cardsTemp[a][language].lore
-            }else{
-                a = Engine.getCardData(a)
-                const name = a.name
-                // achichi%20@Ignister
-                fetch(`https://yugioh.fandom.com/api.php?action=query&titles=${name}&prop=revisions&rvslots=*&rvprop=content&formatversion=2&format=json&origin=*`)
-                    .then(res => res.json())
-                    .then(result => {
-                    var content = result.query?.pages[0]?.revisions[0]?.slots?.main?.content
-
-                    content = content.split('|')
-                    console.log(content)
-                    languageList.forEach(l => {
-                        if(l !== 'en'){
-                            let translate = {
-                                name : content.find(f => f.includes(`${l}_name`))?.split('=')[1]?.trim(),
-                                lore : content.find(f => f.includes(`${l}_lore`))?.split('=')[1]?.trim().replace('\'',''),
-                            }
-
-                            if(cardsTemp[a.id]){
-                                cardsTemp[a.id][l] = translate
-                            }else{
-                                cardsTemp[a.id] = {}
-                                cardsTemp[a.id][l] = translate
-                            }
+                content = content.split('|')
+                console.log(content)
+                languageList.forEach(l => {
+                    if(l !== 'en'){
+                        let translate = {
+                            name : content.find(f => f.includes(`${l}_name`))?.split('=')[1]?.trim(),
+                            lore : content.find(f => f.includes(`${l}_lore`))?.split('=')[1]?.trim().replace('\'',''),
                         }
-                    })
 
-                    localStorage.setItem('cardsTemp', JSON.stringify(cardsTemp));
-                    lastCurrentCardId = 0
-                    loadLanguage(this.currentCardId)
+                        if(cardsTemp[a.id]){
+                            cardsTemp[a.id][l] = translate
+                        }else{
+                            cardsTemp[a.id] = {}
+                            cardsTemp[a.id][l] = translate
+                        }
+                    }
                 })
-            }
+
+                localStorage.setItem('cardsTemp', JSON.stringify(cardsTemp));
+                lastCurrentCardId = 0
+                loadLanguage(this.currentCardId)
+            })
         }
     }
+}
 
-    var cardsTemp = JSON.parse(localStorage.getItem('cardsTemp'));
+var cardsTemp = JSON.parse(localStorage.getItem('cardsTemp'));
 
-    if(!cardsTemp){
-        cardsTemp = {}
-    }
+if(!cardsTemp){
+    cardsTemp = {}
+}
 
-    var language = localStorage.getItem('language')
+var language = localStorage.getItem('language')
 
-    if(!language){
-        language = 'en'
-    }
+if(!language){
+    language = 'en'
+}
 
-    var languageList = ['en','es','fr','de','it','pt'];
+var languageList = ['en','es','fr','de','it','pt'];
 
-    var select = document.createElement("select")
-    select.id = 'language'
-    select.addEventListener('change', changeLanguage)
+var select = document.createElement("select")
+select.id = 'language'
+select.addEventListener('change', changeLanguage)
 
-    languageList.forEach(l => {
-        var option = document.createElement("option")
-        option.value = l
-        option.text = l.toUpperCase()
+languageList.forEach(l => {
+    var option = document.createElement("option")
+    option.value = l
+    option.text = l.toUpperCase()
 
-        select.appendChild(option)
-    })
+    select.appendChild(option)
+})
 
-    select.value = language
+select.value = language
 
-    document.getElementById('editor-menu-content')?.appendChild(select)
+document.getElementById('editor-menu-content')?.appendChild(select)
 
-    var oldSetCardInfo = Engine.UI.prototype.setCardInfo
+var oldSetCardInfo = Engine.UI.prototype.setCardInfo
 
-    var lastCurrentCardId = 0
+var lastCurrentCardId = 0
 
-    Engine.ui.setCardInfo = function(a){
-        oldSetCardInfo.call(this, a);
-        if(language !== 'en') loadLanguage.call(this, a);
-    };
-})();
+Engine.ui.setCardInfo = function(a){
+    oldSetCardInfo.call(this, a);
+    if(language !== 'en') loadLanguage.call(this, a);
+};
